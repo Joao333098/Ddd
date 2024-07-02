e68208bb2e10cd9154e4e59bff71e8f60324c507
Joao333098 <156049113+Joao333098@users.noreply.github.com>
2024-07-02 07:34:37 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  token: 'MTE5MTEzMTYxMDMxMDAwNDc0Ng.GJGFOJ.WhZ27drUGkdqswyD4kkEdIgrov1CHHGKfUMSUE',
+  badWords: [
+    'vend@ cont@', 'vend. cont.', 'vend-cont@', 'vend-conta', 'vend0 cont@', 'vend0 c0nt@',
+    'vendendo c0nt@', 'vend@ c0nta', 'vend@ cont4', 'vend@ cont@', 'vend@ c0nt@',
+    'vend@ c0nt4', 'vend@ c0nt4', 'v e n d @ c o n t @', 'v e n d . c o n t .', 'v e n d - c o n t a',
+    'v e n d o c o n t a', 'v e n d 0 c 0 n t @', 'v e n d a n d o c 0 n t a', 'v e n d 0 c 0 n t a',
+    'v e n d @ c 0 n t a', 'Conta com', 'v e n d @ c 0 n t @', 'v e n d @ c o n t 4', 'v e n d @ c o n t @',
+    'v e n d a n d o c o n t 4', 'v e n d 0 c o n t 4', 'v e n d a n d o c 0 n t 4', 'v e n d 0 c 0 n t 4',
+    'tr0c@ cont@', 'troc@ cont@', 'tr0c0 c0nta', 'trocando c0nt@', 'tr0c@ cont@', 'troc@ c0nt@',
+    'tr0c0 cont.', 'troc. cont.', 'tr0c0 cont-', 'troc- conta', 'tr0c@ c0nta', 'troc@ c0nt4',
+    'tr0c0 cont.', 'trocando cont.', 'tr0c0 cont-', 'trocando c0nta', 'tr0c0 c0nt@', 'trocando c0nt@',
+    'tr0c@ c0nta', 'troc@ cont4', 'troc@ c0nta', 'troc@ cont.', 'trocando cont@', 'trocando c0nt.',
+    'trocando c0nta', 'trocando c0nt-', 'tr0c0 c0nta', 'tr0c0 c0nt.', 'tr0c0 c0nt.', 'tr0c0 c0nt.',
+    'tr0c0 c0nt.', 'troc@ c0nt.', 'troc@ c0nta', 'trocando cont@', 'troc@ cont.', 'troc@ c0nt-',
+    'troc@ cont.', 'troc@ c0nt.', 'troc@ c0nt.', 'tr0c0 c0nta', 'trocando cont.', 'trocando c0nt-',
+    'troc@ c0nt4', 'troc@ cont.',
+    'upo cont@', 'upo c0nt@', 'upo c0nt4', 'upo c0nta', 'upo c0nt@', 'upo c0nt@', 'u p o c o n t @',
+    'up0 conta', 'upando conta', 'up@ conta', 'up. conta', 'up-conta', 'u p o c o n t a',
+    'solo raid por fruta', 'troco conta', 'upo conta'
+  ],
+  allowedRoleId: '1187339962899779584',
+  blockedChannelId: '1250198254059257898'
+}; 
